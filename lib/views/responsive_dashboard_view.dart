8ee97214dd04bf1f76b274785dfc3a8c5dc91d53
MuhammadKhaled <muhammadbenkhaled@gmail.com>
2024-08-05 02:11:45 +0300

    first commit

---
+++ lib/views/responsive_dashboard_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveDashboardView extends StatelessWidget {
+  const ResponsiveDashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
