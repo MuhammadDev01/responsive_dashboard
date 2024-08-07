d602463df4b361c1ac34fb5ad17d0f7eca71d992
MuhammadKhaled <muhammadbenkhaled@gmail.com>
2024-08-07 19:45:29 +0300

    Fix issue with custom drawer

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
-import 'package:responsive_dashboard/utils/app_styles.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer_list_view.dart';
 import 'package:responsive_dashboard/views/widgets/custom_list_tile.dart';
 import 'package:responsive_dashboard/views/widgets/drawer_item.dart';
@@ -12,34 +10,45 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
-      backgroundColor: Colors.white,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 100,
+    return Container(
+      color: Colors.white,
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 100,
+            ),
           ),
-          CustomListTile(
-            image: Assets.imagesAvatar3,
-            title: 'Lekan Okeowo',
-            subtitle: 'demo@gmail.com',
+          SliverToBoxAdapter(
+            child: CustomListTile(
+              image: Assets.imagesAvatar3,
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+            ),
           ),
           DrawerItemsListView(),
-          Expanded(child: SizedBox()),
-          DrawerItem(
-            item: DrawerItemModel(
-              image: Assets.imagesSettings,
-              title: 'Setting system',
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                DrawerItem(
+                  item: DrawerItemModel(
+                    image: Assets.imagesSettings,
+                    title: 'Setting system',
+                  ),
+                ),
+                DrawerItem(
+                  item: DrawerItemModel(
+                    image: Assets.imagesLogout,
+                    title: 'Logout account',
+                  ),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+              ],
             ),
-          ),
-          DrawerItem(
-            item: DrawerItemModel(
-              image: Assets.imagesLogout,
-              title: 'Logout account',
-            ),
-          ),
-          SizedBox(
-            height: 48,
           ),
         ],
       ),
--- lib/views/widgets/custom_drawer_list_view.dart
+++ lib/views/widgets/custom_drawer_list_view.dart
@@ -35,9 +35,7 @@ class _DrawerItemsListViewState extends State<DrawerItemsListView> {
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
+    return SliverList.builder(
       itemBuilder: (context, index) => GestureDetector(
         onTap: () {
           setState(() {
