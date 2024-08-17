4db7c2dcbd4ae3830d46c788a708182b9b718154
MuhammadKhaled <muhammadbenkhaled@gmail.com>
2024-08-17 19:22:45 +0300

    add logic to dots indicator

--- lib/views/widgets/dots_indicator.dart
+++ lib/views/widgets/dots_indicator.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/widgets/active_dot_indicator_builder.dart';
 import 'package:responsive_dashboard/views/widgets/dot_indicator_builder.dart';
 
 class DotsIndicator extends StatelessWidget {
-  const DotsIndicator({super.key});
-
+  const DotsIndicator({super.key, required this.currnetIndex});
+  final int currnetIndex;
   @override
   Widget build(BuildContext context) {
     return Row(
         children: List.generate(
       3,
-      (index) => const Padding(
-        padding: EdgeInsets.only(right: 5),
-        child: DotIndicatorBuilder(),
+      (index) => Padding(
+        padding: const EdgeInsets.only(right: 5),
+        child: currnetIndex == index
+            ? const ActiveDotIndicatorBuilder()
+            : const DotIndicatorBuilder(),
       ),
     ));
   }
--- lib/views/widgets/my_card.dart
+++ lib/views/widgets/my_card.dart
@@ -4,8 +4,30 @@ import 'package:responsive_dashboard/views/widgets/dots_indicator.dart';
 import 'package:responsive_dashboard/views/widgets/my_card_builder.dart';
 import 'package:expandable_page_view/expandable_page_view.dart';
 
-class MyCard extends StatelessWidget {
+class MyCard extends StatefulWidget {
   const MyCard({super.key});
+
+  @override
+  State<MyCard> createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  late int currnetIndex;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    currnetIndex = 0;
+    pageController = PageController();
+
+    pageController.addListener(() {
+      setState(() {
+        currnetIndex = pageController.page!.round();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +42,19 @@ class MyCard extends StatelessWidget {
           height: 20,
         ),
         ExpandablePageView.builder(
+          controller: pageController,
           scrollDirection: Axis.horizontal,
           itemCount: 3,
-          itemBuilder: (context, index) => const MyCardBuilder(),
+          itemBuilder: (context, index) {
+            return const MyCardBuilder();
+          },
         ),
         const SizedBox(
           height: 20,
         ),
-        const DotsIndicator(),
+        DotsIndicator(
+          currnetIndex: currnetIndex,
+        ),
       ],
     );
   }
