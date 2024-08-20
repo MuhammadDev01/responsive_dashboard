021409a2ee9364f2652134c54e694430fe1f50ec
MuhammadKhaled <muhammadbenkhaled@gmail.com>
2024-08-20 03:25:39 +0300

    complete income details

--- lib/views/widgets/income_details.dart
+++ lib/views/widgets/income_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
 import 'package:responsive_dashboard/views/widgets/income_chart_and_details_widget.dart';
 import 'package:responsive_dashboard/views/widgets/income_details_header.dart';
 
@@ -11,13 +12,23 @@ class IncomeDetails extends StatelessWidget {
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(12)),
-      child: const Column(
+      child: Column(
         children: [
-          IncomeDetailsHeader(),
-          SizedBox(
+          const IncomeDetailsHeader(),
+          const SizedBox(
             height: 16,
           ),
-          IncomeChartAndDetailsWidget(),
+          const IncomeChartAndDetailsWidget(),
+          const Divider(
+            height: 56,
+            color: Color(0xffF1F1F1),
+          ),
+          Text(
+            'See detail',
+            style: AppStyles.styleSemiBold16(context).copyWith(
+              color: const Color(0xff4EB7F2),
+            ),
+          ),
         ],
       ),
     );
